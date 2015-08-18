3f7dd538941a07efebab9be416da79303dfdec02
Julia Rowe <Julia@Julias-MacBook-Pro.local>
2015-08-18 10:05:28 -0400

    code to clean up

---
+++ WebProgramming/~messages/lab.js
@@ -0,0 +1,11 @@
+/*function parse() {
+	var request = new XMLHttpRequest();
+	var messages;
+
+	request.open("get", "data.json");
+	request.onreadystatechange = parse;
+
+
+	messages = JSON.parse(request.responseText);
+	return messages;
+}*/
